ac543dc156a29be87605788d81a088d8a1a9b750
Maxime Aubursin <maubursin@ikomobi.com>
2017-06-28 14:34:18 +0200

    Initial commit

---
+++ JPush/Structs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace JPush_iOS
+{
+    [Native]
+    [Flags]
+    public enum JPAuthorizationOptions : int
+    {
+        None = 0,
+        Badge = (1 << 0),
+        Sound = (1 << 1),
+        Alert = (1 << 2)
+    }
+}
